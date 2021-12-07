e80c0282646c50ce3d1364c06fb5087a73e056fc
Chris Arnold <chris@netwealth.com>
2021-12-07 16:23:04 +0000

    feat(day06): solved day 6!

---
+++ AOC2021Day06/LanternFishVisitor.cs
@@ -0,0 +1,44 @@
+﻿namespace AOC2021Day06;
+
+internal static class LanternFishVisitor
+{
+    internal static Dictionary<int, long> VisitForADay(Dictionary<int, long> fish)
+    {
+        const int NewFishCounter = 8;
+        const int FishResetCounter = 6;
+        const int ReadyToSpawnCounter = 0;
+
+        long newFish = 0;
+
+        if (fish.ContainsKey(ReadyToSpawnCounter))
+        {
+            newFish = fish[ReadyToSpawnCounter];
+        }
+
+        var newDictionary = new Dictionary<int, long>();
+
+        for (var i = 1; i <= NewFishCounter; i++)
+        {
+            if (fish.ContainsKey(i))
+            {
+                newDictionary.Add(i - 1, fish[i]);
+            }
+        }
+
+        if (newFish > 0)
+        {
+            newDictionary.Add(NewFishCounter, newFish);
+
+            if (newDictionary.ContainsKey(FishResetCounter))
+            {
+                newDictionary[FishResetCounter] += newFish;
+            }
+            else
+            {
+                newDictionary.Add(FishResetCounter, newFish);
+            }
+        }
+
+        return newDictionary;
+    }
+}

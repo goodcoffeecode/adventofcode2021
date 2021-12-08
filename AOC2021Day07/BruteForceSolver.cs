93775b04b5cfe83dd19e81bc59f9a47b497558cd
Chris Arnold <chris@netwealth.com>
2021-12-08 07:01:41 +0000

    feat(day07): solved day 7!

---
+++ AOC2021Day07/BruteForceSolver.cs
@@ -0,0 +1,39 @@
+﻿namespace AOC2021Day07;
+
+internal class BruteForceSolver
+{
+    private readonly int[] _data;
+
+    internal BruteForceSolver(int[] data)
+    {
+        ArgumentNullException.ThrowIfNull(data);
+
+        _data = data;
+    }
+
+    internal record struct Result(int Position, int Fuel);
+
+    internal Result SolveLinearly() => Solve(Shifter.ShiftDataLinearlyTo);
+
+    internal Result SolveIncreasingly() => Solve(Shifter.ShiftDataIncreasinglyTo);
+
+    private Result Solve(Func<int[],int,int> shifter)
+    {
+        var min = _data.Min();
+        var max = _data.Max();
+
+        var bestResult = new Result(-1, int.MaxValue);
+
+        for (int pos = min; pos < max; pos++)
+        {
+            var fuel = shifter(_data, pos);
+
+            if (fuel < bestResult.Fuel)
+            {
+                bestResult = new Result(pos, fuel);
+            }
+        }
+
+        return bestResult;
+    }
+}
---
+++ AOC2021Day07/Shifter.cs
@@ -0,0 +1,28 @@
+﻿namespace AOC2021Day07;
+
+internal static class Shifter
+{
+    internal static int ShiftDataLinearlyTo(int[] data, int pos)
+    {
+        var fuel = 0;
+
+        for (int d = 0; d < data.Length; d++)
+        {
+            fuel += Math.Abs(data[d] - pos);
+        }
+
+        return fuel;
+    }
+
+    internal static int ShiftDataIncreasinglyTo(int[] data, int pos)
+    {
+        var fuel = 0;
+
+        for (int d = 0; d < data.Length; d++)
+        {
+            fuel += Math.Abs(data[d] - pos).SumOf();
+        }
+
+        return fuel;
+    }
+}
